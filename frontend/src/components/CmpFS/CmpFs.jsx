266aaa4ca2e170fdf520c0d6adb37401e12345ed
Tuba-05 <tubanaushad2005@gmail.com>
2025-09-13 00:53:29 +0500

    Updated some files

--- frontend/src/components/CmpFS/CmpFs.jsx
+++ frontend/src/components/CmpFS/CmpFs.jsx
@@ -14,18 +14,34 @@ function CmpFS() {
   }, [id]); // re-run effect if ID changes
 
   if (!data) return <p>Loading...</p>; // show loading state
+  
+  const latest = data.financials?.[data.financials.length - 1]; // Get the most recent [financials (last entry)]
 
   return (
     <>
-    <h1>HI MATE</h1>
-      {/* Display company financial data */}
-      <div style={{ padding: "20px" }}>
-        <h1>{data.company_name}</h1>
-        <p><b>Revenue:</b> {data.revenue}</p>
-        <p><b>Net Income:</b> {data.net_income}</p>
-        <p><b>Total Assets:</b> {data.total_assets}</p>
-        <p><b>Total Liabilities:</b> {data.total_liabilities}</p>
-      </div>
+    <div style={{ padding: "20px" , marginLeft: "200px",}}>
+      <h1>{data.c_name}</h1>
+      {latest ? (
+        <>
+          <h2>Company Details</h2>
+          <p><b>Symbol:</b> {data.symbol}</p>
+          <p><b>Country:</b> {data.country}</p>
+          <p><b>Price USD:</b> {data.price_usd}$</p>
+          <p><b>Market Capitalization:</b> {data.market_cap}$</p>
+          <p><b>Sector:</b> {data.sector}</p>
+          <h2>Latest Financial Statements</h2>
+          <p><b>Revenue:</b> {latest.revenue$}</p>
+          <p><b>Profit:</b> {latest.profit}$</p>
+          <p><b>Income:</b> {latest.income}$</p>
+          <p><b>Equity:</b> {latest.equity}$</p>
+          <p><b>Assets:</b> {latest.assets}$</p>
+          <p><b>Liabilities:</b> {latest.liabilities}$</p>
+          <p><b>Date:</b> {latest.date}</p>
+        </>
+      ) : (
+        <p>No financial data available</p>
+      )}
+    </div>
     </>
   );
 }
