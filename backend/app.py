8cc2d4ae1487b6e31fb421af0fad887c916e079d
Tuba-05 <tubanaushad2005@gmail.com>
2025-10-12 22:45:27 +0500

    Update some files

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask  # to create flask web app
+from flask import Flask, send_file  # to create flask web app
 from flask_sqlalchemy import SQLAlchemy  # to make Flask talk to database using Python code instead of SQL
 from sqlalchemy import text  #used in line49 unless db connection failed
 # for fetching external data (1.e frontend, APIs) & send proper JSON responses to frontend 
@@ -10,7 +10,9 @@ import requests, base64 # to fetch company logos from Clearbit API & convert to 
 import json  # to read news.json file
 import threading  # to run background tasks (like fetching daily news)
 import sv_daily_news # sv_news.py file (in same folder) for fetching daily news 
-
+import matplotlib.pyplot as plt  # for data visualization 
+import io   # to handle in-memory files 
+import numpy as np
 
 app = Flask(__name__)  # createing flask web application
 
@@ -230,7 +232,7 @@ def get_watchlist(user_id):
             return jsonify({ "success": True, "username": user.name, "companies": [] }) 
         
         company_list = [{ "id": c[0], "c_name": c[1] } for c in companies] # convert to list of dicts 
-        print("Successfully fetched watchlist data from DB") # for checking purposes
+        print("Successfully fetched watchlist data from DB(watchlist)") # for checking purposes
         return jsonify({ "success": True, "username": user.name, "companies": company_list})
 
     except Exception as e:
@@ -274,6 +276,23 @@ def fetch_news_from_file():
         print("Error reading news file:", str(e))
         return jsonify({"success": False, "message": "Error reading news file"}), 500        
 
+
+# ================================== GRAPH ROUTE ========================================
+@app.route('/plot', methods=['GET'])
+def plot_grapg():
+    plt.figure()
+    x = np.array([10, 20, 30, 40, 50, 60, 70, 80, 90, 100])
+    y = np.array([2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023, 2024, 2025])
+    plt.plot(x,y)
+
+    # Save it to a BytesIO object (in memory)
+    img = io.BytesIO()
+    plt.savefig(img, format='png')
+    img.seek(0)
+    plt.close()
+    
+    # Send the image as a response
+    return send_file(img, mimetype='image/png')
 
 # ================== HOME ROUTE ==================
 @app.route("/")
--- frontend/src/components/CmpFS/CmpFs.jsx
+++ frontend/src/components/CmpFS/CmpFs.jsx
@@ -20,76 +20,181 @@ function CmpFS() {
 
   return (
     <>
-    <div style={{ padding: "20px" , marginLeft: "205px", width: '86%', height: '96vh',  marginTop: "18px", position: 'absolute',
-      background: 'linear-gradient( 145deg, #bbebdcff 3%, #babcbcff 20%,  #c2f4e4ff 55%, #838987ff 100%)', 
-      borderRadius: '5px', border: '5px solid #0aa48dff', 
-      boxShadow: '0 4px 8px rgba(0, 0, 0, 1), 0 4px 8px rgba(220, 41, 41, 1)'}} >
-  
-      <div style={{ display: "flex", alignItems: "center", gap: "20px" }}>
-      {/* LOGO + COMPANY NAME */}
-      <img src={`data:image/png;base64,${data.logo}`} alt="Logo" width={50} height={50} />
-      <h1 style={{ fontSize: "75px"}}
-      >{data.c_name}</h1>
-      </div>
-      {latest // if financial data available, show details
-      ? (
-        <>
-        <div style={{ listStyleType: "none", lineHeight: "2", fontfamily: 'Montserrat' }}> 
-          <table>
-            <thead> <tr> <th>
-              <h2 style={{fontSize: "35px"}}>
-                 Details  </h2>
-                </th> </tr> </thead>
-            <tbody>
-              <tr>
-                <td><b><MdArrowRight /> Symbol:</b></td>  <td>{data.symbol}</td>
-              </tr>   <tr>
-                <td><b><MdArrowRight /> Country:</b></td>   <td>{data.country}</td>
-              </tr>   <tr>
-                <td><b><MdArrowRight /> Price USD:</b></td>   <td>{data.price_usd} $</td>
-              </tr>   <tr>
-                <td><b><MdArrowRight /> Market Capitalization: </b></td>   <td>{data.market_cap} $</td>
-              </tr>   <tr>
-                <td><b><MdArrowRight /> Sector:</b></td>  <td>{data.sector}</td>
-              </tr>   <tr>
-                <td><b><MdArrowRight /> Revenue:</b></td>   <td>{latest.revenue} $</td>
-              </tr>   <tr>
-                <td><b><MdArrowRight /> Profit:</b></td>  <td>{latest.profit} $</td>
-              </tr>   <tr>
-                <td><b><MdArrowRight /> Income:</b></td>  <td>{latest.income} $</td>
-              </tr>   <tr>
-                <td><b><MdArrowRight /> Equity:</b></td>  <td>{latest.equity} $</td>
-              </tr>   <tr>
-                <td><b><MdArrowRight /> Assets:</b></td>  <td>{latest.assets} $</td>
-              </tr>   <tr>
-                <td><b><MdArrowRight /> Liabilities:</b></td>   <td>{latest.liabilities} $</td>
-              </tr>   <tr>
-                <td><b><MdArrowRight /> Date:</b></td>  <td>{latest.date}</td>
-              </tr> 
-              </tbody>
-          </table>
-          {/*---------------------------------------------------------------------*/}
-          {/* <li><b><MdArrowRight />Symbol:</b> {data.symbol}</li>
+      <div
+        style={{
+          padding: "20px",
+          marginLeft: "205px",
+          width: "86%",
+          height: "96vh",
+          marginTop: "18px",
+          position: "absolute",
+          background:
+            "linear-gradient( 145deg, #bbebdcff 3%, #babcbcff 20%,  #c2f4e4ff 55%, #838987ff 100%)",
+          borderRadius: "5px",
+          border: "5px solid #0aa48dff",
+          boxShadow:
+            "0 4px 8px rgba(0, 0, 0, 1), 0 4px 8px rgba(220, 41, 41, 1)",
+          flexWrap: "wrap", // allows stacking on smaller screens
+        }}
+      >
+        <div style={{ display: "flex", alignItems: "center", gap: "20px" }}>
+          {/* LOGO + COMPANY NAME */}
+          <img
+            src={`data:image/png;base64,${data.logo}`}
+            alt="Logo"
+            width={50}
+            height={50}
+          />
+          <h1 style={{ fontSize: "75px" }}>{data.c_name}</h1>
+        </div>
+        {latest ? ( // if financial data available, show details
+          <>
+            <div
+              style={{
+                listStyleType: "none",
+                lineHeight: "2",
+                fontfamily: "Montserrat",
+              }}
+            >
+              <table>
+                <thead>
+                  {" "}
+                  <tr>
+                    {" "}
+                    <th>
+                      <h2 style={{ fontSize: "35px" }}>Details </h2>
+                    </th>{" "}
+                  </tr>{" "}
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>
+                      <b>
+                        <MdArrowRight /> Symbol:
+                      </b>
+                    </td>{" "}
+                    <td>{data.symbol}</td>
+                  </tr>{" "}
+                  <tr>
+                    <td>
+                      <b>
+                        <MdArrowRight /> Country:
+                      </b>
+                    </td>{" "}
+                    <td>{data.country}</td>
+                  </tr>{" "}
+                  <tr>
+                    <td>
+                      <b>
+                        <MdArrowRight /> Price USD:
+                      </b>
+                    </td>{" "}
+                    <td>{data.price_usd} $</td>
+                  </tr>{" "}
+                  <tr>
+                    <td>
+                      <b>
+                        <MdArrowRight /> Market Capitalization:{" "}
+                      </b>
+                    </td>{" "}
+                    <td>{data.market_cap} $</td>
+                  </tr>{" "}
+                  <tr>
+                    <td>
+                      <b>
+                        <MdArrowRight /> Sector:
+                      </b>
+                    </td>{" "}
+                    <td>{data.sector}</td>
+                  </tr>{" "}
+                  <tr>
+                    <td>
+                      <b>
+                        <MdArrowRight /> Revenue:
+                      </b>
+                    </td>{" "}
+                    <td>{latest.revenue} $</td>
+                  </tr>{" "}
+                  <tr>
+                    <td>
+                      <b>
+                        <MdArrowRight /> Profit:
+                      </b>
+                    </td>{" "}
+                    <td>{latest.profit} $</td>
+                  </tr>{" "}
+                  <tr>
+                    <td>
+                      <b>
+                        <MdArrowRight /> Income:
+                      </b>
+                    </td>{" "}
+                    <td>{latest.income} $</td>
+                  </tr>{" "}
+                  <tr>
+                    <td>
+                      <b>
+                        <MdArrowRight /> Equity:
+                      </b>
+                    </td>{" "}
+                    <td>{latest.equity} $</td>
+                  </tr>{" "}
+                  <tr>
+                    <td>
+                      <b>
+                        <MdArrowRight /> Assets:
+                      </b>
+                    </td>{" "}
+                    <td>{latest.assets} $</td>
+                  </tr>{" "}
+                  <tr>
+                    <td>
+                      <b>
+                        <MdArrowRight /> Liabilities:
+                      </b>
+                    </td>{" "}
+                    <td>{latest.liabilities} $</td>
+                  </tr>{" "}
+                  <tr>
+                    <td>
+                      <b>
+                        <MdArrowRight /> Date:
+                      </b>
+                    </td>{" "}
+                    <td>{latest.date}</td>
+                  </tr>
+                </tbody>
+              </table>
+              {/*---------------------------------------------------------------------*/}
+              {/* <li><b><MdArrowRight />Symbol:</b> {data.symbol}</li>
           <li><b><MdArrowRight />Country:</b> {data.country}</li>
           <li><b><MdArrowRight />Price USD:</b> {data.price_usd} $</li>
           <li><b><MdArrowRight />Market Capitalization:</b> {data.market_cap} $</li>
           <li><b><MdArrowRight />Sector:</b> {data.sector}nology (IT)</li> */}
-          {/* <h2>Latest Financial Statements</h2> */}
-          {/* <li><b><MdArrowRight />Revenue:</b> {latest.revenue} $</li>
+              {/* <h2>Latest Financial Statements</h2> */}
+              {/* <li><b><MdArrowRight />Revenue:</b> {latest.revenue} $</li>
           <li><b><MdArrowRight />Profit:</b> {latest.profit} $</li>
           <li><b><MdArrowRight />Income:</b> {latest.income} $</li>
           <li><b><MdArrowRight />Equity:</b> {latest.equity} $</li>
           <li><b><MdArrowRight />Assets:</b> {latest.assets} $</li>
           <li><b><MdArrowRight />Liabilities:</b> {latest.liabilities} $</li>
           <li><b><MdArrowRight />Date:</b> {latest.date}</li> */}
-          {/*---------------------------------------------------------------------*/}
-        </div>  
-        </>
-      ) // if no financial data available 
-      : (
-        <p>No financial data available</p>
-      )}
-    </div>
+              {/*---------------------------------------------------------------------*/}
+            </div>
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                marginTop: "20px",
+              }}
+            ></div>
+          </>
+        ) : (
+          // if no financial data available
+          <p>No financial data available</p>
+        )}
+      </div>
     </>
   );
 }
